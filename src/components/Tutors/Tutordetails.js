027aa37404de0d91c277d5f66985f56afe84329b
dharmi <dharmishthaprajapati02@gmail.com>
2023-07-24 17:44:34 +0530

    tutordetails

--- src/components/Tutors/Tutordetails.js
+++ src/components/Tutors/Tutordetails.js
@@ -629,6 +629,21 @@ const Tutordetails = () => {
                     ) : (
                       ""
                     )}
+                     <div
+                    className="gap-2 d-md-flex "
+                    style={{ justifyContent: "end" }}>
+                    { active === "4" ? (
+                      <Link to="/tutorlist">
+                        <button
+                          className="btn btn-outline-primary my-2"
+                          type="button"
+                          onClick={Reactive}>
+                          Verify
+                        </button>
+                      </Link>
+                    ) : (
+                      ""
+                    )}
                     <Link to={`/professionaldetails/${_id}`}>
                       <button
                         className="btn btn-outline-primary mx-1  my-2"
